7452db6b4f0cbee9866475494e30d6fb4da1fe4e
Koko <ktaketa@hotmail.com>
2024-07-21 14:46:24 -0700

    Used inline style tag in html file

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -4,28 +4,10 @@ const game = () => {
     let blockSize = 25;
     let col = 16;
     let row = col;
-    let board;
 
-    // Set up game while window loads
-    window.onload = function () {
-        // Get board element and specify size
-        board = document.getElementById("board");
-        board.height = row * blockSize;
-        board.width = col * blockSize;
-
-        // Create and append div elements to board as blocks
-        for (let i = 0; i < 256; i++) {
-            const div = document.createElement("div");
-            board.appendChild(div);
-        }
-
-        // Genrate food
-        generateFood();
-        document.addEventListener("keyup", changeDirection); // Moving snake
-
-        // Set snake speed
-        setInterval(update, 1000 / 10);
-    };
+    // Access board and style elements
+    let board = document.getElementById("board");
+    let style = document.querySelector("style");
 
     // Game settings
     let gameOver = false;
@@ -35,64 +17,115 @@ const game = () => {
     // Snake
     let snakeX = blockSize * 5;
     let snakeY = blockSize * 5;
-    let snake = [{ snakeX, snakeY }];
+    let snake;
+
+    //Generate snake
+    const snakeBlock = document.createElement("div");
+    snakeBlock.className = "snake";
+    snake = snakeBlock.className;
+
+    snakeBlock.style.backgroundColor = "#49bdfc";
+
+    // Snake size
+    snakeBlock.style.height = `${blockSize}px`;
+    snakeBlock.style.width = `${blockSize}px`;
+    board.appendChild(snakeBlock);
+
+    // Snake position
+    snakeBlock.style.position = "absolute";
+    snakeBlock.style.top = `${snakeY}px`;
+    snakeBlock.style.left = `${snakeX}px`;
 
     // Food
     let foodX = 0;
     let foodY = 0;
-    let apple = [foodX, foodY];
 
     function generateFood() {
-        // Create food element with class of food
-        let food = document.createElement("div");
-        food.className = "food"; 
+        const food = document.createElement("div");
+
+        // Specify food height and width
         food.style.height = `${blockSize}px`;
         food.style.width = `${blockSize}px`;
         food.style.backgroundColor = "#fc5d8d";
         board.appendChild(food);
-    
+
         // Generate random coordinates for food
         foodX = Math.floor(Math.random() * col) * blockSize;
         foodY = Math.floor(Math.random() * row) * blockSize;
-    
+
         // Position food
-        food.style.position = "fixed";
+        food.style.position = "relative";
         food.style.top = `${foodY}px`;
         food.style.left = `${foodX}px`;
-    
-        // Return food
-        return apple;
     }
 
-    // Moving snake
-    let x, y;
+    // Change snake direction
+    let direction = "right";
 
-    function changeDirection(key) {
-        switch(key) {
-            case "ArrowUp" && "W":
-                x = 0;
-                y -= 1;
-                break;
-            
-            case "ArrowDown" && "S":
-                x = 0;
-                y += 1;
-                break;
-            
-            case "ArrowRight" && "D":
-                x += 1;
-                y = 0;
+    function changeDirection(event) {
+        switch (event) {
+            case "ArrowUp":
+            case "w":
+                direction = "up";
                 break;
 
-            case "ArrowLeft" && "A":
-                x -= 1;
-                y = 0;
+            case "ArrowDown":
+            case "s":
+                direction = "down";
+                break;
+
+            case "ArrowRight":
+            case "d":
+                direction = "right";
+                break;
+
+            case "ArrowLeft":
+            case "a":
+                direction = "left";
                 break;
         }
     }
 
+    // Set up game after window object loads
+    window.onload = function () {
+        let height = row * blockSize;
+        let width = col * blockSize;
+        let size = `#board { \n height: ${height}px; \n width: ${width}px;}`;
+        style.append(size);
+
+        generateFood();
+        document.addEventListener("keyup", changeDirection); // Moving snake
+        setInterval(update, 1000 / 10); // Snake speed
+    };
+
     // Update game
-    function update(){}
+    function update() {
+        if (gameOver) {
+            return;
+        }
+
+        // Snake movement
+        switch (direction) {
+            case "up":
+                snakeY -= blockSize;
+                break;
+            case "down":
+                snakeY += blockSize;
+                break;
+            case "right":
+                snakeX += blockSize;
+                break;
+            case "left":
+                snakeX -= blockSize;
+                break;
+        }
+
+        // Reposition snake 
+        snakeBlock.style.top = `${snakeY}px`;
+        snakeBlock.style.left = `${snakeX}px`;
+    }
+
+    update();
 };
 
 game();
